445fd0cbb53cff6813c30e57dcc79252dcb4f5f3
SalvioliAx <65875242+SalvioliAx@users.noreply.github.com>
2025-06-20 13:03:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,6 @@ def get_google_api_key():
 def render_login_page(db):
     """Renderiza a página de login e cadastro."""
     st.title("Bem-vindo ao Analisador-IA ProMax")
-    st.image("https://i.imgur.com/aozL2jD.png", width=120)
     
     login_tab, register_tab = st.tabs(["Login", "Cadastrar"])
 
